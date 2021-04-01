7e4c6565c055d093b352720fd5afecc0deae585e
Boris <kowal.boris@gmail.com>
2021-04-01 07:15:34 +0200

    basic IA added

--- lib/chess_main.rb
+++ lib/chess_main.rb
@@ -8,7 +8,63 @@ class Game
     @colors = %w[white black]
     @players = [Player.new('Player 1', 'white'), Player.new('Player 2', 'black')]
     @current_player_index = 0
-    play
+    choose
+  end
+
+  def choose
+    puts 'Do you want to start a new game or a saved game?'
+    puts '[1] New game against the computer'
+    puts '[2] New game with 2 players'
+    puts '[3] Saved game'
+    input = gets.chomp
+    while !input.match(/[1-3]/) || input.length != 1
+      puts 'please enter a valid choice'.red
+      input = gets.chomp
+    end
+    play_computer if input == '1'
+    play_human if input == '2'
+    load_game if input == '3'
+  end
+
+  def play_computer
+    @new_game.find_available_moves
+    while @new_game.check? == false
+      computer_player
+      switch_player
+      @new_game.find_available_moves
+    end
+    if @new_game.checkmate?(@new_game.king, @new_game.attacker) == true
+      puts 'Checkmate!'.red
+      @new_game.display
+    else
+      puts 'Check!'.red
+      computer_player
+      switch_player
+      play_computer
+    end
+  end
+
+  def computer_player
+    if player == @players[0]
+      one_round_player
+    else
+      one_round_computer
+    end
+  end
+
+  def one_round_computer
+    @new_game.display
+    puts 'Computer playing...'.green
+    sleep(1)
+    attacked = @new_game.attacked_squares('black')
+    key_index = rand(attacked.keys.length)
+    key = attacked.keys[key_index]
+    value_index = rand(attacked[key].length)
+    @new_game.move_pieces(key.position, attacked[key][value_index])
+  end
+
+  def load_game
+    puts 'to be coded'
   end
 
   def other_player_index
@@ -23,20 +79,21 @@ class Game
     @current_player_index = other_player_index
   end
 
-  def play
+  def play_human
     @new_game.find_available_moves
     while @new_game.check? == false
-      one_round(player)
+      one_round_player
       switch_player
       @new_game.find_available_moves
     end
     if @new_game.checkmate?(@new_game.king, @new_game.attacker) == true
-      puts 'Checkmate!'
+      puts 'Checkmate!'.red
+      @new_game.display
     else
-      puts 'Check!'
-      one_round(player)
+      puts 'Check!'.red
+      one_round_player
       switch_player
-      play
+      play_human
     end
   end
 
@@ -44,23 +101,23 @@ class Game
     piece.edges.empty?
   end
 
-  def one_round(player)
+  def one_round_player
     @new_game.display
-    from = get_input_from(player)
+    from = get_input_from
     while no_move?(@new_game.board[from])
-      print 'there is no valid move for this piece. '
-      from = get_input_from(player)
+      print 'there is no valid move for this piece. '.red
+      from = get_input_from
     end
     to = get_input_to
     while @new_game.move_pieces(from, to) == 'Illegal move'
-      puts 'Illegal move'
+      puts 'Illegal move'.red
       to = get_input_to
     end
     @new_game.find_available_moves
     if @new_game.check_color?(player.color)
       puts 'Illegal move, you would be in check!'
       @new_game.reverse_move
-      play
+      play_human
     end
   end
 
@@ -88,11 +145,11 @@ class Game
   end
 
   def get_input_to
-    puts 'Enter the position where you want to move'
+    puts "#{player.name}, enter the position where you want to move"
     input = gets.chomp
     coordinates = @new_game.find_coordinates(input)
     while coordinates.nil?
-      puts 'Enter a valid position'
+      puts 'Enter a valid position'.red
       input = gets.chomp
       coordinates = @new_game.find_coordinates(input)
     end
@@ -100,12 +157,12 @@ class Game
     coordinates
   end
 
-  def get_input_from(player)
-    puts 'Enter the position of the piece you want to move'
+  def get_input_from
+    puts "#{player.name}, enter the position of the piece you want to move"
     input = gets.chomp
     coordinates = @new_game.find_coordinates(input)
     while coordinates.nil? || @new_game.board[coordinates].color != player.color || @new_game.board[coordinates].instance_of?(EmptyCell)
-      puts 'Enter a valid position'
+      puts 'Enter a valid position'.red
       input = gets.chomp
       coordinates = @new_game.find_coordinates(input)
     end
